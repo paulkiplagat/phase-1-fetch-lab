e48833aa9f73df5bbc9d6d7d628ef47ca39a8c04
Paul <paul.kiplagt@student.moringaschool.com>
2022-06-14 19:34:04 +0300

    Done.

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ function fetchBooks() {
   
 }
 
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
